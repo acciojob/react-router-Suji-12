04349acc8612f0ec3db0110fc072ac7e922e1ec7
Suji-12 <23csec23sujithas@gmail.com>
2026-01-04 21:54:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,37 @@
+// import React from "react";
+// import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+// const Home = () => {
+//   return <h2>Welcome to my website!</h2>;
+// };
+
+// const About = () => {
+//   return <h2>This is a sample React Router program.</h2>;
+// };
+
+// const App = () => {
+//   return (
+//     <BrowserRouter>
+//       <ul>
+//         <li>
+//           <Link to="/">Home</Link>
+//         </li>
+//         <li>
+//           <Link to="/about">About</Link>
+//         </li>
+//       </ul>
+
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/about" element={<About />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// };
+
+// export default App;
 import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-
-const Home = () => {
-  return <h2>Welcome to my website!</h2>;
-};
-
-const About = () => {
-  return <h2>This is a sample React Router program.</h2>;
-};
 
 const App = () => {
   return (
@@ -22,8 +46,14 @@ const App = () => {
       </ul>
 
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route
+          path="/"
+          element={<h2>Welcome to my website!</h2>}
+        />
+        <Route
+          path="/about"
+          element={<h2>This is a sample React Router program.</h2>}
+        />
       </Routes>
     </BrowserRouter>
   );
